5514de2760c075bf14b1de211264ac85efb83846
SonMinSeoung <dlgkfn4@naver.com>
2023-05-12 11:08:34 +0900

    페이지로만들기중

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { Title } from './data';
+
 
 function App() {
   return (
@@ -10,7 +12,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="bg-pan-top"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
---
+++ src/data.js
@@ -0,0 +1,107 @@
+export const Title = [{
+    id: 0,
+    name : 'Describing the UI',
+    subhead : [{
+        id : 0,
+        name : 'Your First Component'
+    },{
+        id : 1,
+        name : 'Importing and Exporting Components'
+    },{
+        id : 2,
+        name : 'Writing Markup with JSX'
+    },{
+        id : 3,
+        name : 'JavaScript in JSX with Curly Braces'
+    },{
+        id : 4,
+        name : 'Passing Props to a Component'
+    },{
+        id : 5,
+        name : 'Conditional Rendering'
+    },{
+        id : 6,
+        name : 'Rendering Lists'
+    },{
+        id : 7,
+        name : 'Keeping Components Pure'
+    }]
+},{
+    id : 1,
+    name : 'Adding Interactivity',
+    subhead : [{
+        id : 0,
+        name : 'Responding to Events'
+    },{
+        id : 1,
+        name : "State: A Component's Memory"
+    },{
+        id : 2,
+        name : 'Render and Commit'
+    },{
+        id : 3,
+        name : 'State as a Snapshot'
+    },{
+        id : 4,
+        name : 'Queueing a Series of State Updates'
+    },{
+        id : 5,
+        name : 'Updating Objects in State'
+    },{
+        id : 6,
+        name : 'Updating Arrays in State'
+    }]
+},{
+    id : 2,
+    name : 'Managing State',
+    subhead : [{
+        id : 0,
+        name : 'Reacting to Input with State'
+    },{
+        id : 1,
+        name : 'Choosing the State Structure'
+    },{
+        id : 2,
+        name : 'Sharing State Between Components'
+    },{
+        id : 3,
+        name : 'Preserving and Resetting State'
+    },{
+        id : 4,
+        name : 'Extracting State Logic into a Reducer'
+    },{
+        id : 5,
+        name : 'Passing Data Deeply with Context'
+    },{
+        id : 6,
+        name : 'Scaling Up with Reducer and Context'
+    }]
+},{
+    id : 3,
+    name : 'Escape Hatches',
+    subhead : [{
+        id : 0,
+        name : 'Referencing Values with Refs'
+    },{
+        id : 1,
+        name : 'Manipulating the DOM with Refs'
+    },{
+        id : 2,
+        name : 'Synchronizing with Effects'
+    },{
+        id : 3,
+        name : 'You Might Not Need an Effect'
+    },{
+        id : 4,
+        name : 'Lifecycle of Reactive Effects'
+    },{
+        id : 5,
+        name : 'Separating Events from Effects'
+    },{
+        id : 6,
+        name : 'Removing Effect Dependencies'
+    },{
+        id : 7,
+        name : 'Reusing Logic with Custom Hooks'
+    }]
+}];
